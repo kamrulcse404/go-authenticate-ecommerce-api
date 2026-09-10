8f5bd468d5a4d90dc71056c96af8413351ec3ce4
kamrulcse404 <kamrul.h01960@gmail.com>
2026-09-11 04:23:33 +0600

    config package complete + validation, update .env example & main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"ecommerce-api/internal/platform/config"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"status":"ok"}`))
 	})
 
-	log.Println("server running on :8080")
+	address := fmt.Sprintf(":%s", cfg.Server.Port)
 
-	err := http.ListenAndServe(":8080", mux)
+	log.Printf("server running on %s", address)
+
+	err = http.ListenAndServe(address, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ internal/platform/config/config.go
@@ -0,0 +1,117 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+}
+
+type RedisConfig struct {
+	Host string
+	Port string
+}
+
+type JWTConfig struct {
+	Secret string
+}
+
+type ServerConfig struct {
+	Port string
+}
+
+type Config struct {
+	Server   ServerConfig
+	Database DatabaseConfig
+	Redis    RedisConfig
+	JWT      JWTConfig
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{
+		Server: ServerConfig{
+			Port: getEnv("SERVER_PORT", "8080"),
+		},
+		Database: DatabaseConfig{
+			Host:     os.Getenv("DB_HOST"),
+			Port:     os.Getenv("DB_PORT"),
+			User:     os.Getenv("DB_USER"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Name:     os.Getenv("DB_NAME"),
+			SSLMode:  os.Getenv("DB_SSLMODE"),
+		},
+		Redis: RedisConfig{
+			Host: os.Getenv("REDIS_HOST"),
+			Port: os.Getenv("REDIS_PORT"),
+		},
+		JWT: JWTConfig{
+			Secret: os.Getenv("JWT_SECRET"),
+		},
+	}
+
+	err := cfg.validate()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+
+	if value == "" {
+		return fallback
+	}
+
+	return value
+}
+
+func (c *Config) validate() error {
+
+	if c.Database.Host == "" {
+		return fmt.Errorf("DB_HOST is required")
+	}
+
+	if c.Database.Port == "" {
+		return fmt.Errorf("DB_PORT is required")
+	}
+
+	if c.Database.User == "" {
+		return fmt.Errorf("DB_USER is required")
+	}
+
+	if c.Database.Password == "" {
+		return fmt.Errorf("DB_PASSWORD is required")
+	}
+
+	if c.Database.Name == "" {
+		return fmt.Errorf("DB_NAME is required")
+	}
+
+	if c.Database.SSLMode == "" {
+		return fmt.Errorf("DB_SSLMODE is required")
+	}
+
+	if c.Redis.Host == "" {
+		return fmt.Errorf("REDIS_HOST is required")
+	}
+
+	if c.Redis.Port == "" {
+		return fmt.Errorf("REDIS_PORT is required")
+	}
+
+	if c.JWT.Secret == "" {
+		return fmt.Errorf("JWT_SECRET is required")
+	}
+
+	return nil
+}
